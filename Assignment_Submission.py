04cefa43ac7e5d97ed8b0b7813a3ff42362ec9ee
mallickp <prabir.mallick@gmail.com>
2023-01-17 18:41:53 +0530

    Update Assignment_Submission.py

--- Assignment_Submission.py
+++ Assignment_Submission.py
@@ -1,10 +1,45 @@
-# Given a list param = [14,12,18,63,15], write a program to return the last element.i.e in this case, output should be 15
+#Given is a list of numbers, return the sum of the numbers which are in odd indices (considering that the list starts with index 0)
+#For example: lst = [25,245,58,45,15,85,59,45] then your output should be 420. In this case, the values at odd indexes are 245, 45, 85, 45.
 def question1(param):
     # Write your solution here
-    # return
+    # return 
     pass
-# Given a Sentence, write a program to find the number of characters in it.
+#A company has its product IDs in a list and they want to get the list of only those products IDs whose ID starts with ‘a’ and has a total of 6 (including ‘a’) alpha-numeric characters.
+#Input Specification:
+#input1: List of the product IDs.
+#Sample Input: ["a25648","a5678","457895","65358"]
 def question2(param):
     # Write your solution here
     # return
+    pass
+#Given a String input, calculate and print the number of upper-case letters and lower case letters. Sample String: 'Hello Mr. Rogers, how are you this fine Tuesday?'
+#Expected Output:
+#No. of Upper case characters: 4 No. of Lower case Characters: 33
+def question3(param):
+    # Write your solution here
+    # return 
+    pass
+
+#Two friends (Reetesh, Sumit) went to a restaurant to have dinner. Since Reetesh was the manager of Sumit, Reetesh decided to pay 70% of the total bill while Sumit would pay 30% of the total bill.
+#Given is the dictionary of key-value pair containing item_ordered:price mapping. For example: orderbill = {"chicken65":300,"plaindosa":120}
+#Print the amount that has to paid by Reetesh and Sumit
+#Input Specification:
+#input1: a dictionary of key-value pair containing {"item_ordered":price} mapping.
+#Output Specification:
+#Return a dictionary of key-value pair containing {"reetesh":amount,"sumit":amount} where amount (float) is the money they are going to pay.
+
+def question4(param):
+    # Write your solution here
+    # return
+    pass
+
+#Given is a string of lyrics of a song. Find out how many times the word “welcome” occurs in the lyrics.
+#For example: If given string of lyrics is "I welcome you all to Learn Python along with milesh, welcome you all to our team" then the output should be 2 (integer).
+#Input Specification:
+#A string of lyrics.
+#Output Specification:
+#Integer specifying No. of times word "welcome" appears in the script.
+def question5(param):
+    # Write your solution here
+    # return 
     pass
