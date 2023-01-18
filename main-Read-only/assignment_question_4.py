1dde306b65eaa848007fc481a018120c0d745a85
itsmerajat <114578716+itsmerajat@users.noreply.github.com>
2023-01-18 15:11:06 +0530

    Update assignment_question_4.py

--- main-Read-only/assignment_question_4.py
+++ main-Read-only/assignment_question_4.py
@@ -1,5 +1,9 @@
 import sys
 sys.path.append('./')
 import Assignment_Submission;
-text = input()
-print(Assignment_Submission.question4(text))
+inp = input()
+item_price = {}
+items = inp.replace("{","").replace("}","").replace('"','').split(",")
+for i in items:
+    item_price[i.split(":")[0]] = int(i.split(":")[1])
+print(Assignment_Submission.question4(item_price))
