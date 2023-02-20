d5d18f454e0244720173deac8ec4292f1310cdd7
mallickp <prabir.mallick@gmail.com>
2023-02-20 10:01:46 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,62 @@
+#Given a number x, determine whether the given number is Armstrong number or not. A positive integer of n digits is called an Armstrong number of order n (order is number of digits) if.
+#abcd... = pow(a,n) + pow(b,n) + pow(c,n) + pow(d,n) + ....
+#Input : 153
+#Output : Yes
+#153 is an Armstrong number.
+#1*1*1 + 5*5*5 + 3*3*3 = 153
+#Input : 120
+#Output : No
+#120 is not a Armstrong number.
+#1*1*1 + 2*2*2 + 0*0*0 = 9
+
+def question1(param):
+    # Write your solution here
+   
+    pass
+
+#In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation 
+#Fn = Fn-1 + Fn-2
+#With seed values 
+#F0 = 0 and F1 = 1.
+# Input : 4
+#output : 4*3*2*1 = 24
+def question2(param):
+    # Write your solution here
+  
+    #pass
+#program to check if a string is palindrome or not
+#Examples: 
+#Input : malayalam
+#Output : Yes
+
+#Input : geeks
+#Output : No
+
+def question3(param):
+    # Write your solution here
+ 
+ return 
+
+#Program to find largest element in an array
+#Input : arr[] = {10, 20, 4}
+#Output : 20
+
+
+def question4(param):
+    # Write your solution here
+   
+    pass
+#Program for Sum of squares of first n natural numbers
+
+#Given a positive integer N. The task is to find 12 + 22 + 32 + ….. + N2. Examples:
+#Input : N = 4
+#Output : 30
+#12 + 22 + 32 + 42
+#= 1 + 4 + 9 + 16
+#Input : N = 5
+#Output : 55
+
+def question5(param):
+    # Write your solution here
+    
+    pass
